cd30e64d81c11daa72e1e82ebbcdeb06ca069514
devcauanbernardino <devcauanbernardino@gmail.com>
2025-11-25 16:14:00 -0300

    inicializacao do projeto

---
+++ jogo.js
@@ -0,0 +1,11 @@
+//Definindo o tamanho do jogo (altura e largura da tela)
+let altura = 0
+let largura = 0
+
+ajustaTamanhoPalcoJogo = () => {
+    let altura = window.innerHeight
+    let largura = window.innerWidth
+}
+
+
+
